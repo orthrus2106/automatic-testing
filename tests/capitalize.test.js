705c30032276d88b5b83cc14428017cc350737e9
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-02-25 16:58:26 +0200

    Added node assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,5 @@
 import capitalize from '../src/capitalize.js';
+import { strict as assert } from 'node:assert';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
-
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
-
-console.log('Все тесты пройдены!');
+assert.equal(capitalize(''), '');
+assert.equal(capitalize('hello'), 'Hello');
